43b1e67b4674ce6910a045843555c92cca820233
estebansaez <85312860+estebansaez@users.noreply.github.com>
2022-05-02 01:10:02 -0300

    Add files via upload

---
+++ Automotora-develop/src/main/java/Vendedor.java
@@ -0,0 +1,13 @@
+public class Vendedor {
+
+    private String nombre, rut;
+    private int edad;
+
+    public Vendedor(String nombre, String rut, int edad){
+        this.nombre=nombre;
+        this.rut=rut;
+        this.edad=edad;
+    }
+
+
+}
---
+++ Automotora-develop/src/main/java/main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        Automotora automotora= new Automotora();
+        automotora.probarSistema();
+    }
+}
